2e25b82b52e033668ba3a58654253b15ad4dee8d
danrfiuza <danielfiuza01@gmail.com>
2019-11-05 10:33:30 -0300

    Just testing...

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
+use App\Visitor\Liquor;
+use App\Visitor\Necessity;
 use App\Visitor\TaxVisitor;
 use App\Visitor\Tobacco;
 
 $tax = new TaxVisitor();
 
 $tobacco = new Tobacco(3.50);
+$liquor = new Liquor(2.00);
+$necessity = new Necessity(2.50);
 
 echo $tobacco->accept($tax);
+echo $liquor->accept($tax);
+echo $necessity->accept($tax);
